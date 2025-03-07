6543bb8770097ac72369a89d7d04187d11fa94be
sumeyahussein17 <sumeyahussein21@gmail.com>
2025-03-07 16:42:13 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+
+
+
+function  countDown(number){
+    while(number>=0) {
+        console.log(number);
+        number--;
+    }
+}
+countDown(10)
